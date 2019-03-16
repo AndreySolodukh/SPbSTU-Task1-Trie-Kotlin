35ebb78c87a565790b46629d1874feba26cfbd53
AndreySolodukh <43181564+AndreySolodukh@users.noreply.github.com>
2019-03-16 13:23:03 +0300

    ver. 1.01:  Тесты для разных методов класса распределены по  разным функциям.

--- src/test/kotlin/tasks/Tests.kt
+++ src/test/kotlin/tasks/Tests.kt
@@ -6,20 +6,21 @@ import tasks.Trie.*
 
 class Tests {
 
-    private val trie = Trie(setOf(
-            Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
-            Node('C', setOf(Node('a', setOf(Node('t', setOf())))))
-    ))
 
     @Test
-    fun tests() {
+    fun testadd() {
+
+        val trie = Trie(setOf(
+                Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
+                Node('C', setOf(Node('a', setOf(Node('t', setOf())))))
+        )) // (Dog, Cat)
 
         trie.add("One")
         assertEquals(Trie(setOf(
                 Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
                 Node('C', setOf(Node('a', setOf(Node('t', setOf()))))),
                 Node('O', setOf(Node('n', setOf(Node('e', setOf()))))) // <<<< added
-        )), trie)
+        )), trie) // (Dog, Cat, One)
 
         trie.add("Calc")
         assertEquals(Trie(setOf(
@@ -28,7 +29,7 @@ class Tests {
                 Node('C', setOf(Node('a', setOf(Node('t', setOf()),
                         Node('l', setOf(Node('c', setOf())))))))    // <<<< added
 
-        )), trie)
+        )), trie) // (Dog, One, Cat, Calc)
 
         trie.add("Doge")
         assertEquals(Trie(setOf(
@@ -36,7 +37,7 @@ class Tests {
                 Node('C', setOf(Node('a', setOf(Node('t', setOf()),
                         Node('l', setOf(Node('c', setOf()))))))),
                 Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf()))))))) // <<<<
-        )), trie)
+        )), trie) // (Doge, One, Cat, Calc)
 
         trie.add("Cat")
         assertEquals(Trie(setOf(
@@ -44,46 +45,75 @@ class Tests {
                 Node('C', setOf(Node('a', setOf(Node('t', setOf()),
                         Node('l', setOf(Node('c', setOf()))))))),
                 Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
-        )), trie)
-        // No changes
+        )), trie)  // No changes
+
+        assertTrue(trie.add("Calculate")) // can be added
+        assertFalse(trie.add("Doge")) // cannot be added
+        assertFalse(trie.add("")) // cannot add empty string
+    }
 
 
-        //assertTrue(trie.add("Calculate")) - changes trie, add as last check or in a different fun if needed
-        assertFalse(trie.add("Doge"))
-        assertFalse(trie.add(""))
+    @Test
+    fun testremove() {
+
+        val trie = Trie(setOf(
+                Node('O', setOf(Node('n', setOf(Node('e', setOf()))))),
+                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
+                        Node('l', setOf(Node('c', setOf()), Node('l', setOf()),
+                                Node('e', setOf()), Node('v', setOf()))))))),
+                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
+        )) // (One, Cat, Calc, Call, Cale, Calv, Doge)
 
         trie.remove("One")
         assertEquals(Trie(setOf(
                 // Node('O', setOf(Node('n', setOf(Node('e', setOf()))))),      <<<< removed
                 Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf()))))))),
+                        Node('l', setOf(Node('c', setOf()), Node('l', setOf()),
+                                Node('e', setOf()), Node('v', setOf()))))))),
                 Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
-        )), trie)
+        )), trie) // (Cat, Calc, Call, Cale, Calv, Doge)
 
         trie.remove("Dog")
         assertEquals(Trie(setOf(
                 Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf()))))))),
+                        Node('l', setOf(Node('c', setOf()), Node('l', setOf()),
+                                Node('e', setOf()), Node('v', setOf()))))))),
                 Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
-        )), trie)
-        // No changes.
-
-        trie.add("Call")
-        trie.add("Cale")
-        trie.add("Calv")
-        // Just preparing
+        )), trie) // No changes.
 
         trie.remove("Calc")
         assertEquals(Trie(setOf(
                 Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf()))))))),
                 Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('l', setOf()), Node('e', setOf()),
-                                Node('v', setOf()))))))) // includes "Call", "Cale", "Calv", but not "Calc"
-        )), trie)
+                        Node('l', setOf( /* Node('c', setOf()), */ Node('l', setOf()), // <<<< removed
+                                Node('e', setOf()), Node('v', setOf())))))))
+        )), trie) // (Doge, Cat, Call, Cale, Calv)
+    }
 
-        assertTrue(trie.find("Doge"))
-        assertTrue(trie.find("Ca"))
-        assertFalse(trie.find("Grail"))
+
+    @Test
+    fun testfind() {
+
+        val trie = Trie(setOf(Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
+                Node('C', setOf(Node('a', setOf(Node('t', setOf())))))
+        )) // (Dog, Cat)
+        assertTrue(trie.find("Dog")) // can be found
+        assertTrue(trie.find("Ca")) // can be found
+        assertTrue(trie.find("D")) // can be found
+        assertFalse(trie.find("Doge")) // cannot be found
+        assertFalse(trie.find("Grail")) // cannot be found
+    }
+
+
+    @Test
+    fun testprefix() {
+
+        val trie = Trie(setOf(
+                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf()))))))),
+                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
+                        Node('l', setOf(Node('l', setOf()), Node('e', setOf()),
+                                Node('v', setOf())))))))
+        )) // (Doge, Cat, Call, Cale, Calv)
 
         assertEquals(setOf("Doge"), trie.findPrefix("Dog"))
         assertEquals(setOf<String>(), trie.findPrefix("Detonator"))
@@ -91,8 +121,12 @@ class Tests {
         assertEquals(setOf("Cat", "Call", "Cale", "Calv"), trie.findPrefix("C"))
     }
 
+
     @Test
     fun effectivity() {
+
+        val trie = Trie(setOf())
+
         for (i in 0..10000) trie.add("OMAE_WA_MOU_SHINDEIRU_$i")
         for (i in 0..10000 step 2) trie.remove("OMAE_WA_MOU_SHINDEIRU_$i")
         assertEquals(setOf(
@@ -102,6 +136,6 @@ class Tests {
                 "OMAE_WA_MOU_SHINDEIRU_9627",
                 "OMAE_WA_MOU_SHINDEIRU_9629"
         ), trie.findPrefix("OMAE_WA_MOU_SHINDEIRU_962"))
-    } // Almost 22 secs. Wow.
+    } // It takes about 15-20 secs now. Quite a lot.
 
 }
