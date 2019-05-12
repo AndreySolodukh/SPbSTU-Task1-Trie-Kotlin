47cad955f7c1398b4fa65a41aa99261ed4bf2a62
AndreySolodukh <43181564+AndreySolodukh@users.noreply.github.com>
2019-05-12 21:12:40 +0300

    ver 2.0 - полностью переписан весь класс Trie и тесты к нему

--- src/main/kotlin/tasks/Trie.kt
+++ src/main/kotlin/tasks/Trie.kt
@@ -1,185 +1,94 @@
 package tasks
 
-data class Trie(var trie: Set<Node>) {
 
-    /*
-        Конструкция:
-                                          /---- [C], Set of ...
-                        /---- [A], Set of --- [D], Set of ...
-        [ROOT] Set of: {
-                        \---- [B], Set of --- [E], Set of ...
-
-     */
-
-    data class Node(val value: Char, val next: Set<Node>)
+data class Trie(var trie: Node) {
 
 
-    // *** Вспомогательные функции "add" ***
-    private fun buildNode(str: String): Node {
-        var node = Node(str.last(), setOf())
-        for (i in str.length - 2 downTo 0) node = Node(str[i], setOf(node))
-        return node
-    }
-    private fun delfirst(s: String, num: Int): String = buildString {
-        for (i in num until s.length) this.append(s[i])
-    }
+    data class Node(val value: Char, var next: MutableSet<Node>, var terminal: Boolean) {
 
-    fun add(str: String): Boolean {
 
-        if (str.isEmpty()) return false
-
-        // Нельзя добавить пустую строку. В тестах выдавало ошибку, поэтому пришлось добавить эту проверку.
-
-        if (trie.all { it.value != str.first() }) {
-            trie += buildNode(str)
-            return true
+        private fun buildNode(str: String): Node {
+            var node = Node(str.last(), mutableSetOf(), true)
+            for (i in str.length - 2 downTo 0) node = Node(str[i], mutableSetOf(node), false)
+            return node
         }
 
-        // Если нет веток первого уровня, начинающихся с того же символа, то можно сразу добавить всю строку.
 
-        var root = trie.first { it.value == str.first() }
+        fun addStr(str: String): Boolean {
 
-        // Узел со значением, совпадающим с первым символом строки
+            if (str.isEmpty()) return false
 
-        var other: Map<Int, Set<Node>> = mapOf()
-
-        // Здесь будут храниться все узлы со значениями, не совпадающими с символами строки на их уровне.
-        // Нужно, чтобы потом отстроить новую (дополненную введенной строкой) ветку и заменить ей старую.
-
-        for (i in 1 until str.length) {
-            if (root.next.any { it.value == str[i] }) {
-                other += mapOf(i to root.next.filter { it.value != str[i] }.toSet())
-                root = root.next.first { it.value == str[i] }
-            }
-
-            // Если среди нижних узлов есть такой, что его значение равно следующему символу строки,
-            // то мы сохраняем все остальные нижние узлы и переходим к нужному
-
-            else {
-                root = Node(root.value, root.next + buildNode(delfirst(str, i)))
-
-                // Если больше нет узлов с подходящими значениями, мы создаем новую ветку и добавляем ее на
-                // уровень, с которым работаем сейчас.
-
-                for (j in i - 1 downTo 1)
-                    root = Node(str[j - 1], other[j]!! + root)
-
-                // Дальше мы идем вверх по дереву, внося во все старые ветки информацию
-                // о новом узле. Таким образом доходим до ветки первого уровня.
-
-                trie = trie - trie.filter { it.value == str.first() } + root
+            if (next.all { it.value != str.first() }) {
+                next.add(buildNode(str))
                 return true
-
-                // Полностью удаляем старую ветку первого уровня и ставим вместо нее обновленную.
-
             }
-        }
-        return false
 
-        // Если мы полностью прошли все символы строки в дереве и не нашли ниодного недостающего, то
-        // эта строка уже есть в дереве.
-    }
-
-
-
-    fun remove(str: String): Boolean {
-
-        if (add(str)) return false
-
-        // Если ветку можно добавить, значит ее нет в дереве и ее нельзя удалить.
-
-        var root = trie.first { it.value == str.first() }
-        var other: Map<Int, Set<Node>> = mapOf()
-
-        // См. функцию "add"
-
-        if (str.length == 1)
-            return if (root.next.isEmpty()) {
-                trie -= Node(str.first(), setOf())
-                true
-            } else false
-
-        // Пришлось добавить, т.к. при попытке удалить строку длинной в один символ выдает ошибку.
-
-        for (i in 1 until str.length) {
-            other += mapOf(i to root.next.filter { it.value != str[i] }.toSet())
-            root = root.next.first { it.value == str[i] }
-        }
-
-        // Здесь никаких проверок на наличие символа не нужно, т.к. мы уже установили, что строка есть
-        // в дереве.
-
-        if (root.next.isNotEmpty()) return false
-
-        // Нельзя удалить префикс от строки.
-
-        root = Node(str[other.size - 1], other[other.size]!!)
-
-        // Создаем новую ветку, но уже без последнего символа удаляемой строки.
-
-        for (j in other.size - 1 downTo 1)
-            root = if (root.next.isEmpty())
-                Node(str[j - 1], other[j]!!)
-            else
-                Node(str[j - 1], other[j]!! + root)
-
-        // Как и в "add", вносим изменения в старые ветки. Если с какого-то момента удаляемая строка становится
-        // префиксом, то дальше удалять символы нельзя.
-        // P.S. Вот в этом месте программу можно сделать покрасивее.
-
-        trie = if (root.next.isEmpty())
-            trie - trie.filter { it.value == str.first() }
-        else
-            trie - trie.filter { it.value == str.first() } + root
-
-        // По сути, то же самое действие, что и в цикле выше. Но сейчас изменения вносятся уже в само дерево.
-
-        return true
-    }
-
-
-
-    fun find(str: String): Boolean = !add(str)
-
-
-
-    // *** Вспомогательные функции "findPrefix" ***
-    private fun getchars(n: Node): Set<String> = n.next.map { it.value.toString() }.toSet()
-    private fun take(): Node = trie.first()
-
-    fun findPrefix(str: String): Set<String> {
-
-        if (add(str)) {
-            println("Prefix not found ($str)")
-            return setOf()
-        }
-
-        var root = trie.first { it.value == str.first() }
-
-        for (i in 1 until str.length) {
-            root = root.next.first { it.value == str[i] }
-        }
-
-        var sum = setOf<String>()
-        val tree = Trie(root.next)
-
-        while (getchars(root).any { tree.find(it) }) {
-            val string = buildString {
-                var now = tree.take()
-                append(now.value)
-                while (now.next.isNotEmpty()) {
-                    now = now.next.first()
-                    append(now.value)
-                }
+            if (str.length == 1) {
+                return if (!next.first { it.value == str.first() }.terminal) {
+                    next.first { it.value == str.first() }.terminal = true
+                    true
+                } else false
             }
-            tree.remove(string)
-            sum += str + string
+            return next.first { it.value == str.first() }.addStr(str.drop(1))
         }
 
-        return(sum)
-    }
-    // Самая сложная в плане реализации функция ввиду конструктивных особенностей дерева.
-    // Еще и неэффективная.
-    // И вообще, зачем было так запариваться и возвращать строки, если можно было просто кинуть нужную ветку?
 
+        fun delStr(start: String): Boolean {
+
+            fun deleter(str: String): Boolean {
+
+                if (str.isEmpty()) return false
+
+                if (str.length != 1)
+                    if (next.any { it.value == str.first() }) {
+                        val c = next.first { it.value == str.first() }.terminal     // connected (1) \/
+                        next.first { it.value == str.first() }.delStr(str.drop(1))
+                        if (next.first { it.value == str.first() }.terminal && !c) {
+                            next = next.filter { it.value != str.first() }.toMutableSet()
+                            if (next.isEmpty()) terminal = true     // connected (1) /\ \/
+                        }
+                    } else return false
+
+                if (str.length == 1 && next.any { it.value == str.first() && it.terminal }) {
+                    next.first { it.value == str.first() }.terminal = false
+                    if (next.first { it.value == str.first() }.next.isEmpty()) {
+                        next = next.filter { it.value != str.first() }.toMutableSet() // ! - не работает, если изменить
+                        terminal = true     // connected (1) /\
+                    }
+                    return true
+                } else return false
+            }
+            return deleter(start)
+        }
+
+
+        fun find(str: String): Boolean {
+            if (str.isEmpty()) return false
+            if (next.all { it.value != str.first() }) return false
+            if (str.length == 1 && next.first { it.value == str.first() }.terminal) return true
+            return next.first { it.value == str.first() }.find(str.drop(1))
+        }
+
+
+        private fun pathfinder(str: String): Node? {
+            if (str.isEmpty()) return null
+            if (next.all { it.value != str.first() }) return null
+            if (str.length == 1)
+                return next.first { it.value == str.first() }
+            return next.first { it.value == str.first() }.pathfinder(str.drop(1))
+        }
+        private fun stringer(input: Node, str: String, output: Set<String>): Set<String> {
+            var out = output + input.next.filter { it.terminal }.map { str + it.value}
+            if (input.next.isEmpty()) return out
+            for (elem in input.next) out += stringer(elem, str + elem.value, out)
+            return out
+        }
+
+        fun findPrefix(start: String): Set<String> {
+            var sum = setOf<String>()
+            val path = pathfinder(start) ?: return sum
+            if (find(start)) sum += start
+            return stringer(path, start, sum)
+        }
+    }
 }
--- src/test/kotlin/tasks/Tests.kt
+++ src/test/kotlin/tasks/Tests.kt
@@ -10,132 +10,145 @@ class Tests {
     @Test
     fun testadd() {
 
-        val trie = Trie(setOf(
-                Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf())))))
-        )) // (Dog, Cat)
+        val trie = Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g', mutableSetOf(),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true)), false)), false)
+        ), false) // (Dog, Cat)
 
-        trie.add("One")
-        assertEquals(Trie(setOf(
-                Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()))))),
-                Node('O', setOf(Node('n', setOf(Node('e', setOf()))))) // <<<< added
-        )), trie) // (Dog, Cat, One)
+        trie.addStr("One")
+        assertEquals(Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g', mutableSetOf(),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)), false)), false) // <<<< added
+        ), false), trie) // (Dog, Cat, One)
 
-        trie.add("Calc")
-        assertEquals(Trie(setOf(
-                Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
-                Node('O', setOf(Node('n', setOf(Node('e', setOf()))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf())))))))    // <<<< added
+        trie.addStr("Calc")
+        assertEquals(Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g', mutableSetOf(),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true), Node('l', mutableSetOf(Node('c', mutableSetOf(),
+                        true)), false)), false)), false), // <<<< added
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)),false)),false)
+        ),false).hashCode(), trie.hashCode()) // (Dog, One, Cat, Calc)
 
-        )), trie) // (Dog, One, Cat, Calc)
+        trie.addStr("Doge")
+        assertEquals(Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g',
+                        mutableSetOf(Node('e', mutableSetOf(), true)),
+                        true)), false)), false), // <<<< added
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true), Node('l', mutableSetOf(Node('c', mutableSetOf(),
+                        true)), false)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)), false)), false)
+        ), false).hashCode(), trie.hashCode()) // (Dog, Doge, One, Cat, Calc)
 
-        trie.add("Doge")
-        assertEquals(Trie(setOf(
-                Node('O', setOf(Node('n', setOf(Node('e', setOf()))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf()))))))),
-                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf()))))))) // <<<<
-        )), trie) // (Doge, One, Cat, Calc)
+        trie.addStr("Cat")
+        assertEquals(Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g',
+                        mutableSetOf(Node('e', mutableSetOf(), true)),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true), Node('l', mutableSetOf(Node('c', mutableSetOf(),
+                        true)), false)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)), false)), false)
+        ), false).hashCode(), trie.hashCode())  // No changes
 
-        trie.add("Cat")
-        assertEquals(Trie(setOf(
-                Node('O', setOf(Node('n', setOf(Node('e', setOf()))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf()))))))),
-                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
-        )), trie)  // No changes
-
-        assertTrue(trie.add("Calculate")) // can be added
-        assertFalse(trie.add("Doge")) // cannot be added
-        assertFalse(trie.add("")) // cannot add empty string
+        assertTrue(trie.addStr("Calculate")) // can be added
+        assertFalse(trie.addStr("Doge")) // cannot be added
+        assertFalse(trie.addStr("")) // cannot add empty string
     }
 
 
     @Test
-    fun testremove() {
+    fun testdel() {
+        val trie = Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g',
+                        mutableSetOf(Node('e', mutableSetOf(), true)),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true), Node('l', mutableSetOf(Node('c', mutableSetOf(),
+                        true)), false)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)), false)), false)), false) // (Dog, Doge, One, Cat, Calc)
 
-        val trie = Trie(setOf(
-                Node('O', setOf(Node('n', setOf(Node('e', setOf()))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf()), Node('l', setOf()),
-                                Node('e', setOf()), Node('v', setOf()))))))),
-                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
-        )) // (One, Cat, Calc, Call, Cale, Calv, Doge)
+        trie.delStr("Doge")
+        assertEquals(Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g', mutableSetOf(),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true), Node('l', mutableSetOf(Node('c', mutableSetOf(),
+                        true)), false)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)),false)),false)
+        ),false).hashCode(), trie.hashCode()) // (Dog, One, Cat, Calc)
 
-        trie.remove("One")
-        assertEquals(Trie(setOf(
-                // Node('O', setOf(Node('n', setOf(Node('e', setOf()))))),      <<<< removed
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf()), Node('l', setOf()),
-                                Node('e', setOf()), Node('v', setOf()))))))),
-                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
-        )), trie) // (Cat, Calc, Call, Cale, Calv, Doge)
+        trie.delStr("Calc")
+        assertEquals(Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g', mutableSetOf(),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)), false)), false)
+        ), false).hashCode(), trie.hashCode()) // (Dog, Cat, One)
 
-        trie.remove("Dog")
-        assertEquals(Trie(setOf(
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('c', setOf()), Node('l', setOf()),
-                                Node('e', setOf()), Node('v', setOf()))))))),
-                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf())))))))
-        )), trie) // No changes.
+        trie.delStr("One")
+        assertEquals(Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g', mutableSetOf(),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true)), false)), false)
+        ), false).hashCode(), trie.hashCode()) // (Dog, Cat)
 
-        trie.remove("Calc")
-        assertEquals(Trie(setOf(
-                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf()))))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf( /* Node('c', setOf()), */ Node('l', setOf()), // <<<< removed
-                                Node('e', setOf()), Node('v', setOf())))))))
-        )), trie) // (Doge, Cat, Call, Cale, Calv)
     }
 
 
     @Test
     fun testfind() {
+        val trie = Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g',
+                        mutableSetOf(Node('e', mutableSetOf(), true)),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true), Node('l', mutableSetOf(Node('c', mutableSetOf(),
+                        true)), false)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)), false)), false)), false)
 
-        val trie = Trie(setOf(Node('D', setOf(Node('o', setOf(Node('g', setOf()))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf())))))
-        )) // (Dog, Cat)
-        assertTrue(trie.find("Dog")) // can be found
-        assertTrue(trie.find("Ca")) // can be found
-        assertTrue(trie.find("D")) // can be found
-        assertFalse(trie.find("Doge")) // cannot be found
-        assertFalse(trie.find("Grail")) // cannot be found
+        assertTrue(trie.find("Doge"))
+        assertTrue(trie.find("Dog"))
+        assertTrue(trie.find("Calc"))
+        assertTrue(trie.find("Cat"))
+        assertFalse(trie.find("Pill"))
+        assertFalse(trie.find("Do"))
+        assertFalse(trie.find("C"))
     }
 
 
     @Test
-    fun testprefix() {
+    fun testPrefix() {
+        val trie = Node('*', mutableSetOf(
+                Node('D', mutableSetOf(Node('o', mutableSetOf(Node('g',
+                        mutableSetOf(Node('e', mutableSetOf(), true)),
+                        true)), false)), false),
+                Node('C', mutableSetOf(Node('a', mutableSetOf(Node('t', mutableSetOf(),
+                        true), Node('l', mutableSetOf(Node('c', mutableSetOf(),
+                        true)), false)), false)), false),
+                Node('O', mutableSetOf(Node('n', mutableSetOf(Node('e', mutableSetOf(),
+                        true)), false)), false)), false)
 
-        val trie = Trie(setOf(
-                Node('D', setOf(Node('o', setOf(Node('g', setOf(Node('e', setOf()))))))),
-                Node('C', setOf(Node('a', setOf(Node('t', setOf()),
-                        Node('l', setOf(Node('l', setOf()), Node('e', setOf()),
-                                Node('v', setOf())))))))
-        )) // (Doge, Cat, Call, Cale, Calv)
-
-        assertEquals(setOf("Doge"), trie.findPrefix("Dog"))
-        assertEquals(setOf<String>(), trie.findPrefix("Detonator"))
-        assertEquals(setOf("Call", "Cale", "Calv"), trie.findPrefix("Cal"))
-        assertEquals(setOf("Cat", "Call", "Cale", "Calv"), trie.findPrefix("C"))
+        assertEquals(setOf("One"), trie.findPrefix("One"))
+        assertEquals(setOf("Dog", "Doge"), trie.findPrefix("Do"))
+        assertEquals(setOf("Cat", "Calc"), trie.findPrefix("C"))
+        assertEquals(setOf<String>(), trie.findPrefix("Go"))
     }
-
-
-    @Test
-    fun effectivity() {
-
-        val trie = Trie(setOf())
-
-        for (i in 0..10000) trie.add("OMAE_WA_MOU_SHINDEIRU_$i")
-        for (i in 0..10000 step 2) trie.remove("OMAE_WA_MOU_SHINDEIRU_$i")
-        assertEquals(setOf(
-                "OMAE_WA_MOU_SHINDEIRU_9621",
-                "OMAE_WA_MOU_SHINDEIRU_9623",
-                "OMAE_WA_MOU_SHINDEIRU_9625",
-                "OMAE_WA_MOU_SHINDEIRU_9627",
-                "OMAE_WA_MOU_SHINDEIRU_9629"
-        ), trie.findPrefix("OMAE_WA_MOU_SHINDEIRU_962"))
-    } // It takes about 15-20 secs now. Quite a lot.
-
 }
